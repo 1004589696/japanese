b4c50cc294c6dd19429e876f58358be4c671ad4a
Your Name <ck_ding>
2020-07-14 16:01:42 +0800

    'c'

--- src/pages/m/index/index.vue
+++ src/pages/m/index/index.vue
@@ -1,15 +1,458 @@
 <template>
-  <div>1</div>
+    <div>
+        <img class="top-img" src="./index02.png" />
+        <div class="sub-title">
+            <span>我们的优势</span>
+        </div>
+        <div class="sub-content">
+            <div class="sub-content-title">团队优势</div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />经验丰富、从教多年的骨干教师。
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />对每位学员具有责任心和耐心。秉持着让我们的每位学员都有所收获。
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />教学点位置离大学很近，可以保证每个学员每天只要利用一点课余时间，就能来学习。
+            </div>
+            <div class="sub-content-title">课程优势</div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />阶段性作业演讲，随堂会话练习。 一对一纠错
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />每堂课课后监督检测打卡
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />会给缺课者安排补课
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />不定期团体活动，随时为学生答疑解惑
+            </div>
+            <div class="sub-content-title">时间优势</div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />阶段性作业演讲，随堂会话练习。 一对一纠错
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />上课时间根据大家的课余或业余时间灵活安排
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />缺课者的补课时间也灵活安排
+            </div>
+            <div class="sub-content-content">
+                <img src="./index-advantage-bg04.png" />线上线下随时为学生答疑解惑
+            </div>
+        </div>
+        <div class="sub-title">
+            <span>我们的课程</span>
+        </div>
+        <div class="sub-content2">
+            <div class="kecheng-item">
+                <div class="kecheng-item-img">
+                    <img src="./index03.png" />
+                </div>
+                <div class="kecheng-item-txt">
+                    <div class="kecheng-item-txt-title">日语考研</div>
+                    <div class="kecheng-item-txt-content">突破传统的填鸭式教育，为内部学员提供 实用应试技巧，助学员高分升学。</div>
+                </div>
+            </div>
+            <div class="kecheng-item">
+                <div class="kecheng-item-img">
+                    <img src="./index04.png" />
+                </div>
+                <div class="kecheng-item-txt">
+                    <div class="kecheng-item-txt-title">日语考级</div>
+                    <div
+                        class="kecheng-item-txt-content"
+                    >课程设置具有阶梯螺旋上升的难度递进。针对不同考级要求与内容， 精心研发考点与强化课程。</div>
+                </div>
+            </div>
+            <div class="kecheng-item">
+                <div class="kecheng-item-img">
+                    <img src="./index05.png" />
+                </div>
+                <div class="kecheng-item-txt">
+                    <div class="kecheng-item-txt-title">日语留学游学</div>
+                    <div class="kecheng-item-txt-content">优质的服务，一对一私人订制留学计划。体验日本不同的风土人情。深入感受日本文化。</div>
+                </div>
+            </div>
+            <div class="kecheng-item">
+                <div class="kecheng-item-img">
+                    <img src="./index06.png" />
+                </div>
+                <div class="kecheng-item-txt">
+                    <div class="kecheng-item-txt-title">日语高考</div>
+                    <div class="kecheng-item-txt-content">因材施教，讲究引导式学习方式。关注每个同学的学习情况及状态。</div>
+                </div>
+            </div>
+            <div class="kecheng-item">
+                <div class="kecheng-item-img">
+                    <img src="./index07.png" />
+                </div>
+                <div class="kecheng-item-txt">
+                    <div class="kecheng-item-txt-title">日语口语</div>
+                    <div class="kecheng-item-txt-content">独创的口语教学体系，基础学习与口语学习同步。场景会话演练，让学生克服哑巴日语。</div>
+                </div>
+            </div>
+        </div>
+        <div class="sub-title">
+            <span>我们的师资</span>
+        </div>
+        <swiper :options="{autoplay:{disableOnInteraction:false}}">
+            <swiper-slide>
+                <div class="slider01 slider-01">
+                    <div class="kecheng-item-img">
+                        <img src="./index11.png" />
+                    </div>
+                    <div class="kecheng-item-txt">
+                        <div class="kecheng-item-txt-title">浦田美玲</div>
+                        <div class="kecheng-item-txt-tag">（游学老师）</div>
+                        <div
+                            class="kecheng-item-txt-content"
+                        >私に会うと、みんな元気になります！私に会うと、みんな日本語が上手になります！日本語が上手になって、日本の映画やアニメを見ましょう。本や漫画を読みましょう。歌を歌いましょう。（能力試験もがんばりましょう）強くて、美しい日本語を楽しんで下さい。</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="slider01 slider-02">
+                    <div class="kecheng-item-img">
+                        <img src="./index12.png" />
+                    </div>
+                    <div class="kecheng-item-txt">
+                        <div class="kecheng-item-txt-title">赵老师</div>
+                        <div class="kecheng-item-txt-tag">（游学老师）</div>
+                        <div
+                            class="kecheng-item-txt-content"
+                        >「日本語の勉強は楽しいです」ということ学生に伝えたいので、常に楽しく勉強できる方法を意識して授業をしています。 大学で日本語を専攻して日本語学校の大学院進学コースを通して、日本の大学院で「日本語教育学」を勉強しました。大学院進学までの流れについて何でもお聞きください。 学生として日本語を勉強したことがあるので、学生の気持ちがよくわかります。日本語の質問はもちろん、留学生としての悩みも相談できます。ぜひ日本で楽しく日本語を勉強しましょう。</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="slider01">
+                    <div class="kecheng-item-img">
+                        <img src="./index13.png" />
+                    </div>
+                    <div class="kecheng-item-txt">
+                        <div class="kecheng-item-txt-title">谢老师</div>
+                        <div class="kecheng-item-txt-tag">（留学、游学老师）</div>
+                        <div
+                            class="kecheng-item-txt-content"
+                        >高级讲师、留学、游学老师 ，毕业于湖南工商大学外语系。多年翻译学生留学资料。曾送多名学员出国游学、留学。对于每位想出国的学员，都耐心的讲解有关出国留学、游学的所有事情。执行力非常强。</div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="slider01">
+                    <div class="kecheng-item-img">
+                        <img src="./index14.png" />
+                    </div>
+                    <div class="kecheng-item-txt">
+                        <div class="kecheng-item-txt-title">胡老师</div>
+                        <div class="kecheng-item-txt-tag">（日语初级精讲老师）</div>
+                        <div class="kecheng-item-txt-content">
+                            日语初级精讲、擅长引导零基础学员如何学好日语，独具特色的上课方式让学员在愉快的学习氛围中，打下夯实的基础，为后期的高级日语和考级、考研日语做准备。并且能让学生在短时间内能开口说日语到会基本的日常生活用语。
+                            <br />教龄：2年半
+                        </div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="slider01">
+                    <div class="kecheng-item-img">
+                        <img src="./index15.png" />
+                    </div>
+                    <div class="kecheng-item-txt">
+                        <div class="kecheng-item-txt-title">贺老师</div>
+                        <div class="kecheng-item-txt-tag">（口语老师）</div>
+                        <div class="kecheng-item-txt-content">
+                            毕业于日本九州大学修士，高级口语老师，深受日语文化影响，独具特色的口语练习方式，让学员脱离中文式日语口语。达到工作日语所需要求，并且让学员在口语学习过程中能真正的做到会说到爱说。
+                            <br />教龄：8年
+                        </div>
+                    </div>
+                </div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="slider01 slider-03">
+                    <div class="kecheng-item-img">
+                        <img src="./index16.png" />
+                    </div>
+                    <div class="kecheng-item-txt">
+                        <div class="kecheng-item-txt-title">小松崎 幸隆</div>
+                        <div class="kecheng-item-txt-tag">(游学老师)</div>
+                        <div class="kecheng-item-txt-content">
+                            中央大学文学部史科学東洋史専攻卒業。今では 日本で１０数年 韓国で１０年間 ３０００人以上の学生に日本語を教えてきました。
+                            <br />その間ラジオパーソナリティをやって 6ヶ月日本語を教えました。韓国から日本に戻って ほかの日本語学校で校長をしました。
+                            <br />今東京学社で日本語を教えています。東京学社に入学した皆さんの「夢を叶う」お手伝いをします。
+                            <br />一緒に頑張りましょう。
+                        </div>
+                    </div>
+                </div>
+            </swiper-slide>
+        </swiper>
+        <div class="sub-title">
+            <span>我们的风采</span>
+        </div>
+        <div class="slider-box">
+            <swiper ref="mySwiper" :options="{autoplay:{disableOnInteraction:false}}">
+                <swiper-slide>
+                    <div class="fengcai">
+                        <img src="./index21.png" />
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="fengcai">
+                        <img src="./index22.png" />
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="fengcai">
+                        <img src="./index23.png" />
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="fengcai">
+                        <img src="./index24.png" />
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="fengcai">
+                        <img src="./index25.png" />
+                    </div>
+                </swiper-slide>
+                <swiper-slide>
+                    <div class="fengcai">
+                        <img src="./index26.png" />
+                    </div>
+                </swiper-slide>
+            </swiper>
+            <div class="btn-next" v-if="swiper&&swiper.activeIndex<5" @click="next">NEXT</div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {};
-  }
+    data() {
+        return {
+            swiper: null
+        };
+    },
+    mounted() {
+        this.swiper = this.$refs.mySwiper.$swiper;
+    },
+    methods: {
+        next() {
+            let index = this.swiper.activeIndex;
+            if (index < 5) {
+                this.swiper.slideTo(index + 1, 1000, false);
+            }
+        }
+    }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+.top-img {
+    width: 100%;
+    vertical-align: top;
+}
+
+.sub-title {
+    padding-top: 0.68rem;
+    height: 0.65rem;
+    line-height: 0.65rem;
+    text-align: center;
+    font-size: 0.5333rem;
+    font-family: PingFangSC;
+    font-weight: 600;
+    color: rgba(51, 51, 51, 1);
+    position: relative;
+    span {
+        position: relative;
+        z-index: 1;
+    }
+}
+
+.sub-title::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    margin-left: -0.54rem;
+    width: 1.08rem;
+    height: 0.10666rem;
+    background: linear-gradient(
+        91deg,
+        rgba(0, 138, 255, 1),
+        rgba(0, 204, 255, 1)
+    );
+}
+.sub-content {
+    padding: 0 1rem 0 0.94rem;
+    .sub-content-title {
+        height: 0.6rem;
+        font-size: 0.5333rem;
+        font-family: PingFangSC;
+        font-weight: 600;
+        color: rgba(51, 51, 51, 1);
+        line-height: 0.6rem;
+        padding-top: 0.7333rem;
+        padding-bottom: 0.4rem;
+    }
+    .sub-content-content {
+        font-size: 0.4rem;
+        font-family: PingFangSC;
+        font-weight: 400;
+        color: rgba(51, 51, 51, 1);
+        line-height: 0.6rem;
+        padding-bottom: 0.1rem;
+        img {
+            vertical-align: top;
+            padding-top: 0.07rem;
+            height: 0.46rem;
+            margin-right: 0.2rem;
+        }
+    }
+}
+
+.sub-content2 {
+    .kecheng-item {
+        padding: 0.4rem 0.4rem 0 0.4rem;
+        overflow: hidden;
+        .kecheng-item-img {
+            float: left;
+            width: 4.546667rem;
+            height: 4.64rem;
+            overflow: hidden;
+            img {
+                width: 100%;
+            }
+        }
+        .kecheng-item-txt {
+            margin-left: 5.2rem;
+            .kecheng-item-txt-title {
+                padding-top: 0.653333rem;
+                padding-bottom: 0.59rem;
+                height: 0.46rem;
+                font-size: 0.42rem;
+                font-family: PingFangSC;
+                font-weight: 500;
+                color: rgba(65, 65, 65, 1);
+                line-height: 0.46rem;
+            }
+            .kecheng-item-txt-content {
+                padding-right: 0.253333rem;
+                font-size: 0.32rem;
+                font-family: PingFangSC;
+                font-weight: 400;
+                color: rgba(51, 51, 51, 1);
+                line-height: 0.42rem;
+                text-align: justify;
+            }
+        }
+    }
+    .kecheng-item:first-child {
+        padding-top: 1.4rem;
+    }
+}
+.slider01 {
+    padding: 0.4rem 0.4rem 0 0.4rem;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    .kecheng-item-img {
+        flex: 0 0 4.50666rem;
+        width: 4.50666rem;
+        height: 6.66666rem;
+        overflow: hidden;
+        img {
+            width: 100%;
+        }
+    }
+    .kecheng-item-txt {
+        flex: 1;
+        margin-left: 0.4rem;
+        .kecheng-item-txt-title {
+            padding-bottom: 0.25rem;
+            height: 0.5rem;
+            line-height: 0.5rem;
+            text-align: center;
+            font-size: 0.45rem;
+            font-family: PingFangSC;
+            font-weight: 400;
+            color: rgba(0, 0, 3, 1);
+        }
+        .kecheng-item-txt-tag {
+            padding-bottom: 0.4rem;
+            font-size: 0.26666rem;
+            font-family: PingFangSC;
+            font-weight: 400;
+            color: rgba(0, 0, 3, 1);
+            line-height: 0.3rem;
+            text-align: center;
+        }
+        .kecheng-item-txt-content {
+            padding-right: 0.3rem;
+            font-size: 0.32rem;
+            font-family: PingFangSC;
+            font-weight: 400;
+            color: rgba(102, 102, 102, 1);
+            line-height: 0.4rem;
+            text-align: justify;
+        }
+    }
+}
+.slider01.slider-03 {
+    .kecheng-item-txt-tag {
+        padding-bottom: 0.3rem;
+    }
+    .kecheng-item-txt-content {
+        font-size: 0.293333rem;
+        line-height: 0.35rem;
+        padding-right: 0;
+    }
+}
+.slider01.slider-02 {
+    .kecheng-item-txt-title {
+        padding-bottom: 0.1rem;
+    }
+    .kecheng-item-txt-tag {
+        padding-bottom: 0.1rem;
+    }
+    .kecheng-item-txt-content {
+        font-size: 0.293333rem;
+        line-height: 0.33rem;
+        padding-right: 0;
+    }
+}
+.fengcai {
+    padding-top: 0.8rem;
+    img {
+        width: 100%;
+        vertical-align: top;
+    }
+}
+.slider-box {
+    position: relative;
+    .btn-next {
+        position: absolute;
+        z-index: 1;
+        bottom: 0.5rem;
+        right: 0.2rem;
+        width: 2rem;
+        height: 0.8rem;
+        background: linear-gradient(
+            91deg,
+            rgba(0, 138, 255, 1),
+            rgba(0, 204, 255, 1)
+        );
+        border-radius: 0.4rem;
+        line-height: 0.8rem;
+        font-size: 0.35rem;
+        font-family: PingFangSC;
+        font-weight: 400;
+        color: rgba(255, 255, 255, 1);
+        text-align: center;
+    }
+}
 </style>
